a542a2e5662f6511144a572b400799ac5c5ae456
jmc27 <jmc27@brandeis.edu>
2014-03-11 11:34:07 -0400

    added resources
    title, description, and type to item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Lostandfound::Application.routes.draw do
-  resources :items
+  resources :items do
+    resources :title
+    resources :descrition
+    resources :type
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
