14231f43b178072c012847226df46bc8f1df1683
jmc27 <jmc27@brandeis.edu>
2014-03-11 11:11:41 -0400

    Rails lost and found item app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lostandfound::Application.config.secret_key_base = 'f89ec9fa387844167e7c7b2bac84c76fe37691f28fe16d8681d9ba1bb23483bccffebe803b196fd67b4a582565886128aeddf3245d6bfecf66bfe923b6f4a573'
